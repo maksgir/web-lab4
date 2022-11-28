93666294f17fd9044211fb3259ede255b3898179
maksgir <maksgirya@mail.ru>
2022-11-28 20:36:46 +0300

    user details

---
+++ src/main/java/com/maksgir/webbackend/serizlizer/UserDetailsImpl.java
@@ -0,0 +1,72 @@
+package com.maksgir.webbackend.serizlizer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.maksgir.webbackend.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private long id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    public UserDetailsImpl(long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static UserDetailsImpl built(User user){
+        return new UserDetailsImpl(user.getId(), user.getUsername(), user.getEmail(), user.getPassword());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
