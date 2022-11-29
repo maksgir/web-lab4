3dd7755a13291d7abb465ca669cdd3cfef252829
maksgir <maksgirya@mail.ru>
2022-11-29 22:31:34 +0300

    request mapping fixed

---
+++ frontend/src/app/dto/register-info.ts
@@ -0,0 +1,12 @@
+export class RegisterInfo {
+  name: string;
+  username:string;
+  password:string;
+
+
+  constructor(name: string, username: string, password: string) {
+    this.name = name;
+    this.username = username;
+    this.password = password;
+  }
+}
