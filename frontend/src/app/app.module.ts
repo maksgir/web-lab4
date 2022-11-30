8222eb784fb09359230bb786d4499b3420177d18
maksgir <maksgirya@mail.ru>
2022-11-30 23:13:01 +0300

    material added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
@@ -10,6 +12,9 @@ import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {httpInterceptorProviders} from "./auth/token-injector";
 import { UserComponent } from './user/user.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +22,8 @@ import { UserComponent } from './user/user.component';
     LoginComponent,
     RegistrationComponent,
     HomePageComponent,
-    UserComponent
+    UserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -30,6 +36,9 @@ import { UserComponent } from './user/user.component';
       {path: 'login', component: LoginComponent},
       {path: 'user', component: UserComponent},
     ]),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
