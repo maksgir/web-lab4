998a783912aa72b9b6114026df35812c7310655b
maksgir <maksgirya@mail.ru>
2022-12-04 18:30:02 +0300

    separation App controller on Point and User controllers

---
+++ backend/src/main/java/com/maksgir/webbackend/controller/PointController.java
@@ -0,0 +1,23 @@
+package com.maksgir.webbackend.controller;
+
+
+import com.maksgir.webbackend.dto.PointDTO;
+import com.maksgir.webbackend.service.PointService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/point")
+public class PointController {
+
+    @Autowired
+    private PointService pointService;
+
+    @PostMapping("/save")
+    public void savePoint(@RequestBody PointDTO pointDTO, @AuthenticationPrincipal UserDetails details){
+        pointService.savePoint(pointDTO, details.getUsername());
+    }
+}
---
+++ backend/src/main/java/com/maksgir/webbackend/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.maksgir.webbackend.controller;
+
+import com.maksgir.webbackend.service.PointService;
+import com.maksgir.webbackend.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserServiceImpl userService;
+
+    @GetMapping("/clear")
+    public void clearPoints(@AuthenticationPrincipal UserDetails details){
+        userService.clearPoints(details.getUsername());
+    }
+}
